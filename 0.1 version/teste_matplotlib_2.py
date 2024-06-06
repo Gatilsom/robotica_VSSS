db792da342aead21ec9fb5f4ed019371027266c4
Gatilsom <107775815+Gatilsom@users.noreply.github.com>
2024-06-06 11:02:20 -0300

    Add files via upload

--- 0.1 version/teste_matplotlib_2.py
+++ 0.1 version/teste_matplotlib_2.py
@@ -1,8 +1,9 @@
 from time import time
-import numpy as np
-import matplotlib.pyplot as plt
+
 import matplotlib.animation as ani
-from matplotlib.patches import Rectangle, Circle
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Rectangle
 
 _field_length = 1.5  # comprimento do campo [m]
 _field_width = 1.3  # largura do campo [m]
@@ -21,8 +22,6 @@ _length_wheel = 75 / 1000  # distância entre eixos das rodas [m]
 
 _wheel_left = 30 * np.pi / 180  # velocidade angular da roda esquerda [rad/s]
 _wheel_right = 60 * np.pi / 180  # velocidade angular da roda direita [rad/s]
-
-
 
 print(int(_max_time / _delta_t))
 print("======================")
@@ -63,38 +62,36 @@ class Robot:
         self.vel_angular = 0.0
         self.wheel_radius = wheel_radius
 
-
     def set_position(self, x, y):
         self.x_pos = x - self.length / 2
         self.y_pos = y - self.length / 2
         self.robot.set_xy([self.x_pos, self.y_pos])
 
-
     def set_wheel(self, left, right):  # usar rad/s
         self.left_wheel = left
         self.right_wheel = right
-        self.vel_angular = self.wheel_radius * (self.right_wheel - self.left_wheel) / self.length  # velocidade angular [rad/s]
+        self.vel_angular = self.wheel_radius * (
+                self.right_wheel - self.left_wheel) / self.length  # velocidade angular [rad/s]
         self.vel_linear = self.wheel_radius * (self.right_wheel + self.left_wheel) / 2  # velocidade linear [m/s]
-
 
     def get_velocity_robot(self):
         return [self.vel_linear, self.vel_angular]
 
-
-    def get_velocity_org(self):
-        # velocidades do robô em relação ao frame inercial
-        _theta = 30 * np.pi / 180  # orientação em relação ao frame inercial [rad]
-        _ine_angular_robot = _angular_robot  # [rad/s]
-        _ine_linear_robot_x = _linear_robot * np.cos(_theta)  # em relação ao eixo x [m/s]
-        _ine_linear_robot_y = _linear_robot * np.sin(_theta)  # em relação ao eixo y [m/s]
+    """
+        def get_velocity_org(self):
+            # velocidades do robô em relação ao frame inercial
+            _theta = 30 * np.pi / 180  # orientação em relação ao frame inercial [rad]
+            _ine_angular_robot = _angular_robot  # [rad/s]
+            _ine_linear_robot_x = _linear_robot * np.cos(_theta)  # em relação ao eixo x [m/s]
+            _ine_linear_robot_y = _linear_robot * np.sin(_theta)  # em relação ao eixo y [m/s]
+    """
 
     def get_position(self):
         return [self.x_pos + self.length / 2, self.y_pos + self.length / 2]
 
-
-
-
     # função inicial chamada no início da plotagem
+
+
 def init_plot():
     global _robot_capsule, _robot_capsule2
     global _time_reference, _time
